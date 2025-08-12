b41f25ce0b71b4dac42aa501bd0414ff4fd04864
Pranshu Kashyap <145654871+pranshukashyap@users.noreply.github.com>
2025-08-12 22:28:55 +0530

    Backend uploaded
    Used Java SpringBoot, MongoDB

---
+++ movies/src/test/java/charcoal/red/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package charcoal.red.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
